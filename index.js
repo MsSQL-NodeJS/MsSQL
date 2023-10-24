9bc755fbcc39a8cec4ba71aa5ca1a3e4856cfd17
AnasAlkhamis <anas.alkhamnis@gmail.com>
2023-10-24 15:59:20 +0300

    setup server

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+require("dotenv").config();
+const app = express();
+app.use(express.json());
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`Server Running On Port ${PORT}`);
+});
